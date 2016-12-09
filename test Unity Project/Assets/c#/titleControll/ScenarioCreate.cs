e6be8466daa7c00f548ec6e3c5ebd5fbb6660ae1
Yuzo Yamamuro <k014c1287@it-neec.jp>
2016-12-09 13:59:01 +0900

    紐づけと思われるファイルを片っ端からコミット

--- test Unity Project/Assets/c#/titleControll/ScenarioCreate.cs
+++ test Unity Project/Assets/c#/titleControll/ScenarioCreate.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class ScenarioCreate : MonoBehaviour {
 
 	public void onClick(){
-		Debug.Log("createonClicked!");
 		Application.OpenURL("http://example.com");
 	}
 }
