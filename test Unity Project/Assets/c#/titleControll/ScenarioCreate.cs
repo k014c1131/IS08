b68ac6c377d19baa32964dcfaa8f0d4b76e61561
Yuzo Yamamuro <k014c1287@it-neec.jp>
2016-12-09 13:45:19 +0900

    タイトル画面のボタンを実装。 ScenarioSelectを呼び出すメソッドと、example.comへと飛ぶメソッドを実装済み。 残りのメソッドが実装されたタイミングで更新する予定。

---
+++ test Unity Project/Assets/c#/titleControll/ScenarioCreate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenarioCreate : MonoBehaviour {
+
+	public void onClick(){
+		Debug.Log("createonClicked!");
+		Application.OpenURL("http://example.com");
+	}
+}
---
+++ test Unity Project/Assets/c#/titleControll/scenarioplayButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class scenarioplayButton : MonoBehaviour {
+
+	public void onClick(){
+		SceneManager.LoadScene("scenarioSelect");
+	}
+}
