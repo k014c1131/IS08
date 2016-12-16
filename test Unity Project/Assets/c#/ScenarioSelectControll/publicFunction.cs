48974e17238b1b55e549a416ebaadc3190b7a382
Yuzo Yamamuro <k014c1287@it-neec.jp>
2016-12-16 15:59:40 +0900

    作業の一時保存

---
+++ test Unity Project/Assets/c#/ScenarioSelectControll/publicFunction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using model;
+using System.Collections.Generic;
+
+public class publicFunction : MonoBehaviour {
+	public List<scenarioData> scenarios = new List<scenarioData>();
+	// Use this for initialization
+	void Start () {
+		//DBにデータを問い合わせる処理
+		string result = "DBにデータを問い合わせる処理";
+		//シナリオデータの数だけjを繰り返す
+		for(int j = 0;j < 0 ;j++){
+			string name = "テストシナリオデータ";
+			List<item> items = new List<item>();
+			//アイテムの数だけiを繰り返す
+			for (int i = 0;i < 0 ;i++){
+				string item_name = "アイテムテストデータ";
+				string pic = "../../吉田君.jpg";
+				string[] introduction = new string[]{"アイテム説明","文章","テスト"};
+				string target_id = "testdigestid";
+				items.Add(new item(item_name,pic,introduction,target_id));
+			}
+			string[] prorogs_text = new string[]{"1行目","2行目","3行目","4行目"};
+			string[] epiloge_text = new string[]{"1行目","2行目","3行目","4行目"}; 
+			TimeSpan limit_time = new TimeSpan(1, 0, 0);
+			scenarios.Add(new scenarioData(name,items,prorogs_text,epiloge_text,limit_time));
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ test Unity Project/Assets/c#/model.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace model{
+public class model : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class item : MonoBehaviour {
+ 	private string name{ get; set; }				//アイテムの名前
+ 	private string pic{ get; set; } 				//アイテムのARマーカーの画像ターゲットID
+ 	private string[] introduction{ get; set; } 		//アイテムの説明文
+ 	private string target_id { get; set; }
+ 	public item(string name,string pic,string[] introduction,string target_id){
+ 		this.name = name;
+ 		this.pic = pic;
+ 		this.introduction = introduction;
+ 		this.target_id = target_id;
+ 	}	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+public class scenarioData : MonoBehaviour {
+	private string name{ get; set; }
+	private List<item> items{ get; set; }
+	private string[] prorogs_text{ get; set; }
+	private string[] epiloge_text{ get; set; }
+	private TimeSpan limit_time{ get; set; }
+	public scenarioData(string Name,List<item> items,string[] prorogs_text,string[] epiloge_text,TimeSpan limit_time){
+		this.name = name;
+		this.items = items;
+		this.prorogs_text = prorogs_text;
+		this.epiloge_text = epiloge_text;
+		this.limit_time = limit_time;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+}
